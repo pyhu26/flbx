d887a235c4d70c8bf1c7283022722a0e27d4ad6d
pyhu26 <pyhu26@gmail.com>
2020-11-03 07:59:53 +0900

    filter interface, 부모 클래스 추가

---
+++ FLBX/Constants/ColSpan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FLBX
+{
+    public class ColSpan
+    {
+        public ColSpan(int mdSpanValue, int? smSpanValue, int? lgSpanValue)
+        {
+            if (mdSpanValue <0 || mdSpanValue > 5)
+                throw new ArgumentOutOfRangeException("mdSpanValue between 0~5");
+
+            if (smSpanValue != null && (smSpanValue < 0 || smSpanValue > 5))
+                throw new ArgumentOutOfRangeException("smSpanValue between 0~5");
+
+            if (lgSpanValue != null && (lgSpanValue < 0 || lgSpanValue > 5))
+                throw new ArgumentOutOfRangeException("lgSpanValue between 0~5");
+
+            string cssColSpan = @"col-";
+            this.ColSpanSMValue = cssColSpan + "sm-" + smSpanValue.ToString();
+            this.ColSpanMDValue = cssColSpan + "md-" + mdSpanValue.ToString();
+            this.ColSpanLGValue = cssColSpan + "lg-" + lgSpanValue.ToString();
+            
+        }
+
+        public string ColSpanSMValue { get; set; }
+        public string ColSpanMDValue { get; set; }
+        public string ColSpanLGValue { get; set; }
+    }
+}
---
+++ FLBX/Interfaces/IFxComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FLBX
+{
+    public interface IFxComponent
+    {
+
+        public string AuthorityLevel { get; set; }
+        public ColSpan ColSpanValue { get; set; }
+        public void GetValue();
+        public void SetRegion();
+        public void SetAuthority();
+            
+    }
+}
