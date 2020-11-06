b73a2591dcaf64eb80e8d9827d366cef73ba5d16
yanshifu <yanshifu2012@hotmail.com>
2020-11-06 10:12:16 +0900

    modify lothistory

--- FLBX/Components/FxDateRangePicker.cs
+++ FLBX/Components/FxDateRangePicker.cs
@@ -51,7 +51,7 @@ namespace FLBX.Components
         }
 
         public string AuthorityLevel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public ColSpan ColSpanValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+     
 
         public void GetValue()
         {
